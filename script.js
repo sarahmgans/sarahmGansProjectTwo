70b28faa5668b624f09e3616640add1a81ef7e92
sarahmgans <sarah.gans@hotmail.com>
2020-04-01 12:44:45 -0400

    hamburger

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
 
 // Attach an event listener onto the icon element in the header, and on click, toggle between having the vertical menu visible and not visible, as well as between having the hamburger icon and the x icon. 
 $('i.home').on('click', function(){
-    $('nav').toggleClass('navVisible');
-    $('i.home').toggleClass('fa fa-bars fas fa-times');
+    $('.hamburger-menu nav').toggleClass('navVisible')
+    $('i.home').toggleClass('fa fa-bars fas fa-times')
+    // $('header').toggleClass('overlay');
+    // $('h1').toggleClass('navVisible')
 })
+
+
+
 });
