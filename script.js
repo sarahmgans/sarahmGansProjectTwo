523299c9c57377b97214137dc3ecda07b412206e
sarahmgans <sarah.gans@hotmail.com>
2020-04-01 16:53:43 -0400

    made the hamburger vertical

--- script.js
+++ script.js
@@ -2,19 +2,10 @@
 $(document).ready(function () {
 
 // Attach an event listener onto the icon element in the header, and on click, toggle between having the vertical menu visible and not visible, as well as between having the hamburger icon and the x icon. 
-$('i.home').on('click', function(){
-    $('.hamburger-menu nav').toggleClass('navVisible')
-    $('i.home').toggleClass('fa fa-bars fas fa-times')
-})
 
-$('i.blog').on('click', function () {
+$('i.move').on('click', function(){
     $('.hamburger-menu nav').toggleClass('navVisible')
-    $('i.blog').toggleClass('fa fa-bars fas fa-times')
-})
-
-$('i.contact').on('click', function () {
-    $('.hamburger-menu nav').toggleClass('navVisible')
-    $('i.contact').toggleClass('fa fa-bars fas fa-times')
+    $('i.move').toggleClass('fa fa-bars fas fa-times')
 })
 
 });
