d0ef84f0570217e69e047bbfea2fc2e518cdb16e
sarahmgans <sarah.gans@hotmail.com>
2020-04-01 20:19:23 -0400

    fixing the text input for mobile

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Begin with a document ready that will be holding all of the js
 $(document).ready(function () {
 
-// Attach an event listener onto the icon element in the header, and on click, toggle between having the vertical menu visible and not visible, as well as between having the hamburger icon and the x icon. 
-
+// Attach an event listener onto the icon element in the header, and on click, toggle between having the vertical menu visible and not visible, as well as between having the hamburger icon visible and the x icon visible. 
 $('i.move').on('click', function(){
     $('.hamburger-menu nav').toggleClass('navVisible')
     $('i.move').toggleClass('fa fa-bars fas fa-times')
